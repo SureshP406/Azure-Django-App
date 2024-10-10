3bd74bf66018d2dc388c4d6bd3662ebd9186125c
SureshP406 <suresh.ece406@gmail.com>
2024-10-10 17:10:58 +0530

    update wsgi

--- ventrure_view/wsgi.py
+++ ventrure_view/wsgi.py
@@ -11,9 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ventrure_view.settings')  ##Default/Original setting
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ventrure_view.settings')  ##Default/Original setting
 
-settings_module = 'ventrure_view.production' if 'WEBSITE_HOSTNAME' in os.environ else 'ventrure_view.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+# settings_module = 'ventrure_view.production' if 'WEBSITE_HOSTNAME' in os.environ else 'ventrure_view.settings'
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
