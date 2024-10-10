ded74e90118fbe553b216ef40c669e8cc466526f
SureshP406 <suresh.ece406@gmail.com>
2024-10-10 12:00:02 +0530

    Create sample django app with production deployment settings

---
+++ ventrure_view/manage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ventrure_view.settings')  #Default/Oroginal settings
+    
+    settings_module = 'venture_view.production' if 'WEBSITE_HOSTNAME' in os.environ else 'venture_view.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
